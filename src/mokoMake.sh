b28e40201849caee76f934b122edeee92004c8d5
jakob <jakob@883a673f-1a8a-466a-bc00-8af087681498>
2008-07-08 11:03:48 +0000

    - Added makefile to build for the moko
    git-svn-id: svn://localhost/moko_eightball@2 883a673f-1a8a-466a-bc00-8af087681498

---
+++ src/mokoMake.sh
@@ -0,0 +1,10 @@
+#!bin/sh
+valac --pkg gtk+-2.0 --pkg cairo -C *.vala
+
+arm-angstrom-linux-gnueabi-gcc `pkg-config --cflags gtk+-2.0 cairo` -c MokoEightBall.c -o MokoEightBall.o
+
+arm-angstrom-linux-gnueabi-gcc `pkg-config --cflags gtk+-2.0 cairo` -c EightBallSurface.c -o EightBallSurface.o
+
+arm-angstrom-linux-gnueabi-gcc `pkg-config --libs gtk+-2.0 cairo`  MokoEightBall.o EightBallSurface.o -o MokoEightBall
+
+rm *.c *.h
