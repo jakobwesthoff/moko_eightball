b902835289f45f0137248384ff4521973737402e
jakob <jakob@883a673f-1a8a-466a-bc00-8af087681498>
2008-07-12 22:49:19 +0000

    - Adapted moko makefile to compile the dataset classes
    git-svn-id: svn://localhost/moko_eightball@17 883a673f-1a8a-466a-bc00-8af087681498

--- src/mokoMake.sh
+++ src/mokoMake.sh
@@ -3,7 +3,7 @@
 
 valac --thread --pkg gtk+-2.0 --pkg cairo --pkg gio-2.0 -C *.vala
 
-for i in MokoEightBall EightBallSurface AccelerometerEventManager AccelerometerEvent; do
+for i in MokoEightBall EightBallSurface AccelerometerEventManager AccelerometerEvent AccelerometerEventData; do
 	arm-angstrom-linux-gnueabi-gcc `pkg-config --cflags gtk+-2.0 gio-2.0 cairo gthread-2.0` -c $i.c -o $i.o
 done;
 
