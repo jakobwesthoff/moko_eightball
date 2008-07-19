be3a16f80862e24363a75643bef40d36944cfc71
jakob <jakob@883a673f-1a8a-466a-bc00-8af087681498>
2008-07-19 23:09:35 +0000

    - Added proper licenses to every file # The repository will become public after this commit
    git-svn-id: svn://localhost/moko_eightball@35 883a673f-1a8a-466a-bc00-8af087681498

--- python/src/eightball.py
+++ python/src/eightball.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python
 
+############################################################################### 
+# 
+# Moko Eightball main application 
+# 
+# This file is part of Moko Eightball. 
+# 
+# Moko Eightball is free software; you can redistribute it and/or modify 
+# it under the terms of the GNU General Public License as published by 
+# the Free Software Foundation; version 3 of the License. 
+# 
+# Moko Eightball is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+# GNU General Public License for more details. 
+# 
+# You should have received a copy of the GNU General Public License 
+# along with arbit; if not, write to the Free Software 
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA 
+# 
+############################################################################### 
+
 import random
 import math
 import evas
