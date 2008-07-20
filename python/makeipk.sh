1b5198ca0878d204c7180fa8d03cc1e74273f7fe
jakob <jakob@883a673f-1a8a-466a-bc00-8af087681498>
2008-07-20 21:35:31 +0000

    - Added ipk creation script
    git-svn-id: svn://localhost/moko_eightball@38 883a673f-1a8a-466a-bc00-8af087681498

---
+++ python/makeipk.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cp src/eightball.py ipk/usr/bin/eightball
+chmod u+x ipk/usr/bin/eightball
+
+cp src/Accelerometer/*.py ipk/usr/share/moko_eightball/Accelerometer/
+
+cd src/data/themes
+./maketheme.sh
+cd ../../../
+
+cp src/data/themes/eightball.edj ipk/usr/share/moko_eightball/eightball.edj
+
+chown -R root:root ./ipk
+
+ipkg-build ipk ./
+
